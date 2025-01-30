6ad1aaa4b56868a78e7adc6c6c8b86fcf069720e
shrmohit <161517182+shrmohit@users.noreply.github.com>
2025-01-30 23:39:48 +0530

    static data m and f

--- staticdatamemberandfunction/staticdatamember.cpp
+++ staticdatamemberandfunction/staticdatamember.cpp
@@ -1,15 +1,48 @@
+// they are attributes of classes or class member
+// it is declared using staic keyword
+// only one copy of that member is created for the entire class and is shared by all the object
+// static member function
 #include <iostream>
 using namespace std;
 
 class Customer
 {
     string name;
-    int acct_balance, balance;
+    int acct_number, balance;
+    // static data member
+    static int total_Customer;
+    static int total_acct_balance;
 
-    Customer()
+public:
+    Customer(string name, int acct_number, int balance)
     {
+        this->name = name;
+        this->acct_number = acct_number;
+        this->balance = balance;
+        total_acct_balance += balance;
+        total_Customer++;
+    }
+    // static member function
+    static void accesStatic()
+    {
+        cout << "Total Customer" << total_Customer << endl;
+        cout << "Total accunt balance" << total_acct_balance << endl;
+    }
+
+    void display()
+    {
+        cout << name << " " << acct_number << " " << balance << endl;
     }
 };
+// initialize the static member
+int Customer::total_acct_balance = 0;
+int Customer::total_Customer = 0;
+
 int main()
 {
+    Customer A1("Mohit", 1, 1000);
+    Customer A2("Rohit", 2, 1000);
+    Customer::accesStatic();
+    A1.display();
+    A2.display();
 }
